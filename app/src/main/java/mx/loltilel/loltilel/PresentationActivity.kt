5115d3668b2aaaced8ba6e232f42788e65caf029
Norman Sánchez <44387183+normansanchezn@users.noreply.github.com>
2020-08-22 00:58:42 -0500

    [ADD] Se creó la UI para la pantalla de bienvenida

--- app/src/main/java/mx/loltilel/loltilel/PresentationActivity.kt
+++ app/src/main/java/mx/loltilel/loltilel/PresentationActivity.kt
@@ -2,10 +2,19 @@ package mx.loltilel.loltilel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_presentation.*
 
 class PresentationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_presentation)
+
+        btnConocenos.setOnClickListener {
+            Toast.makeText(
+                this,
+                "Estamos trabajando en esta funcionalidad, espera nuevas actualizaciones", Toast.LENGTH_SHORT)
+                .show()
+        }
     }
 }
